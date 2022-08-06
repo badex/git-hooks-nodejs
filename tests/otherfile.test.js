c521036fe1484c5512b5f1794732e50a908a15dd
Alex Barrett <badex@users.noreply.github.com>
2022-08-06 02:09:02 +0100

    Removed otherfile.test.js

--- tests/otherfile.test.js
+++
@@ -1,6 +0,0 @@
-const otherfile = require('../src/otherfile');
-
-it('should return correct otherfile', () => {
-  const result = otherfile();
-  expect(result).toBe('String from other-file.js');
-});
